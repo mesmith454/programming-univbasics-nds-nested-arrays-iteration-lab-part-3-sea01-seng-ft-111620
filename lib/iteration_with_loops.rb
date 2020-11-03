289c637790c70151449cb2dd3906586320680ea3
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-03 20:28:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count do
-      if mixed_data[row_index][element_index].is_s?
+      if mixed_data[row_index][element_index].class = String
         sentence << mixed_data[row_index][element_index]
       end
       element_index += 1 
